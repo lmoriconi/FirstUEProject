c7b0cea9b85431117d441b777d8de1f2be6ed086
lmoriconi <lenny.moriconi@gmail.com>
2018-10-11 14:52:14 +0200

    [11/10/2018] Level1

---
+++ Source/FirstProjectUE/FirstProjectUE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FirstProjectUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstProjectUE, "FirstProjectUE" );
